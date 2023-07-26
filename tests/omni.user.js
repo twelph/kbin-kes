bcc9e301452a6e7c15661f747c58b0b9ba9300d4
aclist <92275929+aclist@users.noreply.github.com>
2023-07-26 13:21:58 +0900

    chore: bump version

--- tests/omni.user.js
+++ tests/omni.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BBB-debug
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://kbin.social/*
